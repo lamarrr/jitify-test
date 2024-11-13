7db804457ea041a8890dab1904b427315de02e18
Basit Ayantunde <rlamarrr@gmail.com>
2024-11-13 14:46:09 +0000

    updated build script

--- build.sh
+++ build.sh
@@ -1,17 +1,31 @@
-CUDA_TOOLKIT_DIR="$(pkg-config --cflags cudart-12.6)"
+#!/usr/bin/env bash
+CUDA_TOOLKIT_FLAGS="$(pkg-config --cflags cudart-12.6)"
 
-rm -r jitify
-rm -r cccl
-rm -r rmm
+SOURCE_DIR=${PWD}
+
+
+rm -r ${SOURCE_DIR}/jitify
+rm -r ${SOURCE_DIR}/rmm
+
+cd ${SOURCE_DIR}
 git submodule update --init --recursive
-cd ./jitify && git apply ../jitify.patch
-cd ../rmm && git apply ../rmm.patch
-cd ..
 
-rm -r build
-mkdir build
-cd build
-cmake ../jitify -GNinja 
-cd ..
+cd ${SOURCE_DIR}/jitify
+git apply ${SOURCE_DIR}/jitify.patch
 
-./build/jitify2_preprocess  ./kernel1.cu -I${PWD}/rmm/include/ ${CUDA_TOOLKIT_DIR} -I/usr/include -std=c++17 
+cd ${SOURCE_DIR}/rmm
+git apply ${SOURCE_DIR}/rmm.patch
+
+rm -r ${SOURCE_DIR}/build
+mkdir ${SOURCE_DIR}/build
+cp -r ${SOURCE_DIR}/jitify ${SOURCE_DIR}/build/jitify-src
+cp -r ${SOURCE_DIR}/rmm ${SOURCE_DIR}/build/rmm-src
+
+mkdir ${SOURCE_DIR}/build/jitify-build
+cmake -S ${SOURCE_DIR}/build/jitify-src -GNinja -B ${SOURCE_DIR}/build/jitify-build
+cd ${SOURCE_DIR}/build/jitify-build
+ninja
+
+cd ${SOURCE_DIR}
+
+${SOURCE_DIR}/build/jitify-build/jitify2_preprocess  ./kernel1.cu -I${SOURCE_DIR}/build/rmm-src/include/ ${CUDA_TOOLKIT_FLAGS} -I/usr/include -std=c++17
