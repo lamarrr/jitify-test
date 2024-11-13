3859fca90226b6fb43678b50af99d6a9f5976448
Basit Ayantunde <rlamarrr@gmail.com>
2024-11-13 14:13:05 +0000

    🚧 update

--- build.sh
+++ build.sh
@@ -1,3 +1,17 @@
 CUDA_TOOLKIT_DIR="$(pkg-config --cflags cudart-12.6)"
 
+rm -r jitify
+rm -r cccl
+rm -r rmm
+git submodule update --init --recursive
+cd ./jitify && git apply ../jitify.patch
+cd ../rmm && git apply ../rmm.patch
+cd ..
+
+rm -r build
+mkdir build
+cd build
+cmake ../jitify -GNinja 
+cd ..
+
 ./build/jitify2_preprocess  ./kernel1.cu -I${PWD}/rmm/include/ ${CUDA_TOOLKIT_DIR} -I/usr/include -std=c++17 
